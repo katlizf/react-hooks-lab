4f729210728c0d0a467960e2e426ee63682e31d2
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-06-20 17:04:38 -0500

    set-up done, app-1 completed

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import List from './components/List'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [todos, setTodos] = useState([])
+
+    const addTodo = item => {
+        const newTodos = [...todos, item]
+        setTodos(newTodos)
+    }
+
+    return (
+        <div className="App">
+            <AddTodo addTodo={addTodo} />
+            <List list={todos} />
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ app-1/src/components/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Todo({item}) {
+
+
+    return (
+        <div>
+            <p>{item}</p>
+        </div>
+    )
+}
+
+export default Todo
